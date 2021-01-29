3464afb30a3b9f7a7309e5f5cd002ab933ef1f9e
student-mcast-MSD61a <56297786+student-mcast-MSD61a@users.noreply.github.com>
2021-01-29 13:55:50 +0100

    Implemented the Grid Script
    Implemented the Grid Script, along with changing the Camera Size to Orthographic, 50.

---
+++ Scripts/Testing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    Grid grid;
+    int value;
+
+    private void Start()
+    {
+        grid = new Grid(100, 100, 10f, new Vector3(20, 0));
+    }
+
+}
